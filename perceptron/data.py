a09fdb1ae90bfaabe6a11c184faff65effde3c95
Francisco Andrade <frma059@gmail.com>
2025-05-14 11:25:49 -0300

    decouple result from main data model fields; abstract dot product calc from target and source functions

--- perceptron/data.py
+++ perceptron/data.py
@@ -1,59 +1,42 @@
 from collections import namedtuple
+from typing import Sequence
 import numpy as np
 
-seed = 1
-rng = np.random.default_rng(seed=seed)
-
-Credit = namedtuple(
-    "Credit", ["threshold", "wealth_score", "profile_score", "debt_score", "result"]
-)
-
-source_parameters = [-0.7, 1.0, 1.0, 1.0]
+Credit = namedtuple("Credit", ["threshold", "wealth_score", "profile_score", "debt_score"])
 
 
-def f(data_point: Credit) -> bool:
-    return sum(
-        [
-            source_parameters[0] * data_point.threshold,
-            source_parameters[1] * data_point.wealth_score,
-            source_parameters[2] * data_point.profile_score,
-            source_parameters[3] * data_point.debt_score,
-        ]
+source_parameters = [-1.0, 1.0, 1.0, 1.0]
+
+
+def dot_product(x: Sequence[float], y: Sequence[float]):
+    return sum([a * b for a, b in zip(x, y)])
+
+
+def f(data_point: Sequence[float]) -> bool:
+    assert len(data_point) == len(source_parameters)
+    return dot_product(x=data_point, y=source_parameters)
+
+
+def training_data(
+    rng: np.random.Generator,
+    dimensionality: int = 4,
+    sample_size: int = 1000,
+):
+    avg = 0.3
+    std = 0.1
+
+    thresholds = [1.0] * sample_size
+    data = list(
+        map(
+            lambda x: (Credit(*x), f(x) >= 0),
+            zip(
+                thresholds,
+                *[
+                    rng.normal(loc=avg, scale=std, size=sample_size)
+                    for _ in range(1, dimensionality)
+                ],
+            ),
+        )
     )
 
-
-scale = 1
-dimensionality = 4
-
-sample_size = 10000
-
-
-# wealth_score = rng.normal(0.5 * scale, std, sample_size)
-# profile_score = rng.normal(0.5 * scale, std, sample_size)
-
-avg = 0.3
-std = 0.1
-
-
-thresholds = [1.0] * sample_size
-wealth_score = rng.normal(loc=avg, scale=std, size=sample_size)
-profile_score = rng.normal(loc=avg, scale=std, size=sample_size)
-debt_score = rng.normal(loc=avg, scale=std, size=sample_size)
-
-data = list(
-    map(
-        lambda x: Credit(
-            *x,
-            result=sum(
-                [
-                    x[0] * source_parameters[0],
-                    x[1] * source_parameters[1],
-                    x[2] * source_parameters[2],
-                    x[3] * source_parameters[3],
-                ]
-            )
-            >= 0
-        ),
-        zip(thresholds, wealth_score, profile_score, debt_score),
-    )
-)
+    return data
--- perceptron/main.py
+++ perceptron/main.py
@@ -1,40 +1,40 @@
-from data import Credit, data, rng, dimensionality, f
+from typing import Sequence
+import numpy as np
+from data import Credit, dot_product, f, training_data, source_parameters
+
+
+seed = 1
+dimensionality = len(Credit._fields)
+sample_size = 100000
+rng: np.random.Generator = np.random.default_rng(seed=seed)
 
 target_parameters = [rng.random() for _ in range(dimensionality)]
 
 
-def g(data_point: Credit) -> bool:
-    return sum(
-        [
-            target_parameters[0] * data_point.threshold,
-            target_parameters[1] * data_point.wealth_score,
-            target_parameters[2] * data_point.profile_score,
-            target_parameters[3] * data_point.debt_score,
-        ]
-    )
+def g(data_point: Sequence[float]) -> bool:
+    assert len(data_point) == len(target_parameters)
+    return dot_product(x=data_point, y=target_parameters)
 
 
 done = False
 i = 0
 
+data = training_data(rng=rng, dimensionality=dimensionality, sample_size=sample_size)
+
 while not done:
     i += 1
-    for idx, row in enumerate(data):
-        test = g(row)
-        control = f(row)
+    for idx, (data_point, result) in enumerate(data):
+        test = g(data_point=data_point)
+        control = f(data_point=data_point)
+        assert result == (control >= 0.0)
 
         test_result = test >= 0.0
-        control_result = control >= 0.0
 
-        assert row.result == control_result
-
-        if test_result != control_result:
-            y = 1 if test < control else -1  # * f(row)
-
-            target_parameters[0] += y * row.threshold
-            target_parameters[1] += y * row.wealth_score
-            target_parameters[2] += y * row.profile_score
-            target_parameters[3] += y * row.debt_score
+        if test_result != result:
+            y = 1 if test < control else -1
+            # y = control - test
+            for idx, value in enumerate(data_point._asdict().values()):
+                target_parameters[idx] += y * value
             break
 
         if idx == len(data) - 1:
@@ -42,3 +42,12 @@ while not done:
 
 print(f"Processed after {i} iterations!")
 print(target_parameters)
+print(source_parameters)
+
+print(
+    dot_product(target_parameters, source_parameters)
+    / (
+        np.sqrt(dot_product(target_parameters, target_parameters))
+        * np.sqrt(dot_product(source_parameters, source_parameters))
+    )
+)
